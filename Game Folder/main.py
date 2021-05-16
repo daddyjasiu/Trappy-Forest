1a5ca33ef5cdbaba95220d6a094fb37932232171
unknown <jasiu.skwarczek@gmail.com>
2021-05-16 22:17:28 +0200

    added basic main menu

--- Game Folder/main.py
+++ Game Folder/main.py
@@ -2,16 +2,36 @@ import pygame
 import sys
 
 ############################################################
+# GAME FUNCTIONS:
+############################################################
+def drawMenu():
+    background = pygame.image.load('assets/menu/menuBG.jpg')
+    background = pygame.transform.scale(background, (1400, 720))
+
+    welcomeSign = pygame.image.load('assets/menu/welcome.png')
+    returnSign = pygame.image.load('assets/menu/press_return.png')
+
+    screen.blit(background, (0, 0))
+    screen.blit(welcomeSign, (65, 100))
+    screen.blit(returnSign, (180, 400))
+
+def drawFirstLevel():
+    screen.fill((255, 255, 255))
+
+############################################################
 # GAME INIT:
 ############################################################
 pygame.init()
-width = 400
-height = 600
+pygame.display.set_caption('Trappy Forest')
+width = 1400
+height = 720
 screen = pygame.display.set_mode((width, height))
+clock = pygame.time.Clock()
 
 ############################################################
-# IMPORTS:
+# GAME VARIABLES:
 ############################################################
+gameActive = False
 
 ############################################################
 # GAME MAIN LOOP:
@@ -21,4 +41,17 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RETURN and not gameActive:
+                gameActive = True
+            if event.key == pygame.K_ESCAPE and not gameActive:
+                pygame.quit()
+                sys.exit()
+
+        if not gameActive:
+            drawMenu()
+        if gameActive:
+            drawFirstLevel()
+
     pygame.display.update()
+    clock.tick(120)

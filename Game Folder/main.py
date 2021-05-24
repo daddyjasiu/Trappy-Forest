ae8ad16eee2401a64826243796cc16ff96e9f6fe
hi-im-angel <jasiu.skwarczek@gmail.com>
2021-05-24 20:19:06 +0200

    design patterns

--- Game Folder/main.py
+++ Game Folder/main.py
@@ -2,25 +2,10 @@ import pygame
 import sys
 
 ############################################################
-# GAME FUNCTIONS:
+# MODEL/DAO
 ############################################################
-def drawMenu():
-    background = pygame.image.load('assets/menu/menuBG.jpg')
-    background = pygame.transform.scale(background, (1400, 720))
 
-    welcomeSign = pygame.image.load('assets/menu/welcome.png')
-    returnSign = pygame.image.load('assets/menu/press_return.png')
-
-    screen.blit(background, (0, 0))
-    screen.blit(welcomeSign, (65, 100))
-    screen.blit(returnSign, (200, 400))
-
-def drawFirstLevel():
-    screen.fill((255, 255, 255))
-
-############################################################
 # GAME INIT:
-############################################################
 pygame.init()
 pygame.display.set_caption('Trappy Forest')
 width = 1400
@@ -28,10 +13,32 @@ height = 720
 screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 
-############################################################
 # GAME VARIABLES:
-############################################################
 gameActive = False
+
+# IMPORTING ASSETS:
+menuBackground = pygame.image.load('assets/menu/menuBG.jpg')
+menuBackground = pygame.transform.scale(menuBackground, (1400, 720))
+
+menuWelcome = pygame.image.load('assets/menu/welcome.png')
+menuPressReturn = pygame.image.load('assets/menu/press_return.png')
+
+############################################################
+# VIEW-MODEL
+############################################################
+
+
+
+############################################################
+# VIEW
+############################################################
+def drawMenu():
+    screen.blit(menuBackground, (0, 0))
+    screen.blit(menuWelcome, (65, 100))
+    screen.blit(menuPressReturn, (200, 400))
+
+def drawFirstLevel():
+    screen.fill((255, 255, 255))
 
 ############################################################
 # GAME MAIN LOOP:

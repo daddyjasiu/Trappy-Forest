71b3ed291c42616805578c8fd636758f6b166000
hi-im-angel <jasiu.skwarczek@gmail.com>
2021-06-01 01:07:50 +0200

    some files upload

---
+++ Game Folder/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Game Folder',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Jasiu',
+    author_email='jasiu.skwarczek@gmail.com',
+    description='Trappy Forest'
+)
